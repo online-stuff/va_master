9e3442c9237cfe462667ca3597e5a3d3f211c818
root <root@DEBIAN-7-64-TEMPLATE>
2017-02-23 08:38:25 +0000

    Removed advertise dir from consul.

--- va_master/cli_environment.py
+++ va_master/cli_environment.py
@@ -67,7 +67,7 @@ def write_consul_conf(ip):
     json_conf = {
         'datacenter': 'dc1',
         'data_dir': '/usr/share/consul',
-        'advertise_addr': ip,
+#        'advertise_addr': ip,
         'bootstrap_expect': 1,
         'server': True
     }
