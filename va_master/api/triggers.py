b1787f8b7fc869564264f37466ad124e8a6dc4ed
root <root@DEBIAN-7-64-TEMPLATE>
2017-03-19 21:16:46 +0000

    Added some mockup functions for the triggers.

--- va_master/api/triggers.py
+++ va_master/api/triggers.py
@@ -8,18 +8,49 @@ def add_trigger(handler):
 #        "status" : "OK", 
 #        "conditions" : [
 #        {
-#            "op1" : "get_cpu", 
-#            "op2" : 8, 
-#            "cond" : "<"
+#            "operand1" : "get_cpu", 
+#            "operand2" : 8, 
+#            "operator" : ">"
 #        }, 
 #        {
-#            "op1" : "get_memory", 
-#            "op2" : 8, 
-#            "cond" : "<=",
+#            "operand1" : "get_memory", 
+#            "operand2" : 8, 
+#            "operator" : ">=",
 #        }],
-#        "action" : {
-#                "func" : "set_status", 
+#        "actions" : [
+#            {
+#                "func" : "evo_manager.add_stats", 
 #                "args" : [1]
-#        }   
+#            },
+#        ]
 #    }
+    
+    triggers = yield handler.config.deploy_handler.get_triggers(handler.data['hostname'])
+    triggers.append(handler.data['new_trigger'])
+
+    hosts = yield handler.config.deploy_handler.get_hosts()
+    host = [x for x in hosts if x['hostname'] == handler.data['hostname']][0]
+    host['triggers'] = triggers
+
+    yield handler.config.deploy_handler.datastore.insert('hosts', hosts)
+
+
+@tornado.gen.coroutine
+def receive_trigger(handler):
     host, driver = yield handler.config.deploy_handler.get_host_and_driver(handler.data['hostname'])
+    
+    triggers = yield handler.config.deploy_handler.get_triggers(handler.data['hostname'])
+    trigger = [x for x in triggers if x['service'] == handler.data['service'] and x['status'] == handler.data['status']]
+
+    if not trigger: 
+        raise Exception('No trigger for service ' + handler.data['service'] + ' and status ' + handler.data['status']
+
+    trigger = trigger[0]
+
+    cl = salt.client.LocalClient()
+    results = {}
+    for action in trigger['actions']:
+        result = cl.cmd('evo-master', action['func'], action['args'])
+        results.update(result)
+
+    raise tornado.gen.Return(result)
