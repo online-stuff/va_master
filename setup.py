3b848499928d8c44cb80178518c7797d1bca1115
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2017-06-19 11:38:43 +0200

    Add dashboard build workflow (make)

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@ import setuptools
 setuptools.setup(
   name='vapourapps',
   packages=setuptools.find_packages(),
-  version='0.0.11',
+  version='1.0.0',
   description='This package contains the master server of VapourApps, a DevOps tool for corporate apps.',
   keywords=['vapourapps'],
   author='Filip Dimitrovski',
   install_requires=[
-    'tornado',
     'salt',
-    'apache-libcloud',
     'python-novaclient',
     'pbkdf2', 
     'pyVmomi', #for vmware
-    'libvirt-python', #for, well, libvirt
     'gitpython',
     'watchdog', 
     'clc-sdk',
@@ -24,7 +21,6 @@ setuptools.setup(
   entry_points = {
     'console_scripts': [
         'vapourapps = va_master.cli:entry',
-        'vapourapps-test = va_master.tests:run_tests'
     ]
   }
 )
