65b8bd747312ca090391e7c4ee2046c6a29390e7
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2017-07-18 10:41:08 +0200

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import setuptools
 
 setuptools.setup(
-  name='vapourapps',
-  packages=setuptools.find_packages(),
-  version='1.0.0',
-  description='This package contains the master server of VapourApps, a DevOps tool for corporate apps.',
-  keywords=['vapourapps'],
-  author='Filip Dimitrovski',
-  install_requires=[
-    'salt',
-    'python-novaclient',
-    'pbkdf2', 
-    'pyVmomi', #for vmware
-    'gitpython',
-    'watchdog', 
-    'clc-sdk',
-    'google-api-python-client',
-  ],
-  zip_safe=False,
-  entry_points = {
-    'console_scripts': [
-        'vapourapps = va_master.cli:entry',
-    ]
-  }
+    name='vapourapps',
+    packages=setuptools.find_packages(),
+    version='1.0.0',
+    description='This package contains the master server of VapourApps, a \
+DevOps tool for corporate apps.',
+    keywords=['vapourapps'],
+    author='VapourApps',
+    install_requires=[
+        'salt',
+        'python-novaclient',
+        'pbkdf2',
+        'pyVmomi',
+        'gitpython',
+        'watchdog',
+        'clc-sdk',
+        'google-api-python-client',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'vapourapps = va_master.cli:entry',
+        ]
+    }
 )
