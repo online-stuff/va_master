25537bc7e10682e240a11519cf226ac9e4c68831
staci <staci@va.mk>
2017-10-16 12:17:25 +0000

    Removed unused driver.

--- va_master/deploy_handler.py
+++ va_master/deploy_handler.py
@@ -5,8 +5,7 @@ import traceback
 import functools
 import tornado
 import tornado.gen
-from host_drivers import openstack, aws, vcloud, libvirt_driver, generic_driver, century_link, gce, vmware, aek_driver
-
+from host_drivers import openstack, aws, vcloud, libvirt_driver, generic_driver, century_link, gce, vmware
 
 from Crypto.PublicKey import RSA
 from concurrent.futures import ProcessPoolExecutor
@@ -84,7 +83,6 @@ class DeployHandler(object):
                 gce.GCEDriver,
                 generic_driver.GenericDriver,
                 aws.AWSDriver,
-                aek_driver.AEKDriver,
             ]]
             kwargs['flavours'] = va_flavours
 
