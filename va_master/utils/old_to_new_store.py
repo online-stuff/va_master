9db58c7c12face98d782faebf5444554a87c6bf7
markododa <marko@lugola.net>
2018-03-25 17:31:05 +0200

    Update old_to_new_store.py
    Use relative path

--- va_master/utils/old_to_new_store.py
+++ va_master/utils/old_to_new_store.py
@@ -8,7 +8,7 @@ from va_master.consul_kv.datastore import ConsulStore
 import tornado.ioloop
 
 datastore = ConsulStore()
-datastore_handler = DatastoreHandler(datastore, '/opt/va_master/va_master/consul_kv/consul_spec.json')
+datastore_handler = DatastoreHandler(datastore, '../consul_kv/consul_spec.json')
 
 run_sync = tornado.ioloop.IOLoop.instance().run_sync
 
