b084c364051006998008e1b63b2ea98312dfed09
markododa <marko@lugola.net>
2018-03-26 12:44:57 +0200

    Update config.py
    Use relative paths

--- va_master/va_master_project/config.py
+++ va_master/va_master_project/config.py
@@ -7,6 +7,7 @@ from va_master.consul_kv import datastore
 from va_master.host_drivers import openstack
 
 from va_master.handlers import datastore_handler, drivers_handler
+folder_pwd = os.path.join(os.path.dirname(os.path.realpath(__file__)), '')
 
 def get_server_static():
     # get the server assets static path
@@ -33,10 +34,10 @@ class Config(object):
         self.ssh_key_path = '/root/.ssh/'
         self.ssh_key_name = 'va-master' 
 
-        self.https_crt = '/opt/va_master/ssl/cert.crt'
-        self.https_key = '/opt/va_master/ssl/server.key'
+        self.https_crt = folder_pwd + '../../ssl/cert.crt'
+        self.https_key = folder_pwd + '../../ssl/server.key'
 
-        self.datastore_handler = datastore_handler.DatastoreHandler(datastore = self.datastore, datastore_spec_path = '/opt/va_master/va_master/consul_kv/consul_spec.json')
+        self.datastore_handler = datastore_handler.DatastoreHandler(datastore = self.datastore, datastore_spec_path = folder_pwd+ '../consul_kv/consul_spec.json')
         self.drivers_handler = drivers_handler.DriversHandler(self.datastore_handler, ssh_key_path = self.ssh_key_path, ssh_key_name = self.ssh_key_name)
 
         # Now dynamically inject any kwargs
