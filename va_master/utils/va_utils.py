3a6a8e93686abe7bc5a02aea5b35f4e1d2b150b7
root <root@va-master.glufonec.tk>
2018-05-17 13:12:19 +0200

    Fixes to aws billing

--- va_master/utils/va_utils.py
+++ va_master/utils/va_utils.py
@@ -1,4 +1,14 @@
 prefixes = {'KB' : 10, 'MB' : 20, 'GB' : 30, 'TB' : 40}
+mebi = ['KiB', 'MiB', 'GiB', 'TiB']
+
+def prefix_to_int(bytes, prefix):
+    val = float(bytes) * (2 ** prefixes[prefix])
+    return val
+
+def mebi_to_int(bytes, prefix):
+    mebi_index = mebi.index(prefix) + 1
+    val = float(bytes) * (10 ** (mebi_index * 3))
+    return val
 
 def int_to_bytes(i):
     if not type(i) in [int, float]:
@@ -8,14 +18,19 @@ def int_to_bytes(i):
     prefix = 'GB'
     b = float(i) / (2 ** prefixes[prefix])
     b = '%.2f' % (b) + ' ' + prefix
-
     return b
 
 def bytes_to_int(b):
-    if not type(b) in [str]:
+    print ('Trying to convert ', b, ' to int. ')
+    if not type(b) in [str, unicode]:
         return b
     b = b.split(' ')
-    i = float(b[0]) * (2 ** prefixes[b[1]])
+    if b[1] in prefixes: 
+        i = prefix_to_int(b[0], b[1])
+    elif b[1] in mebi: 
+        i = mebi_to_int(b[0], b[1])
+    else:
+        raise Exception('Cannot convert ' + str(b) + ' to int. ')
 
     return i
 
